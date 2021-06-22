49604b7e101bc22164b4e3bec781dadabc3149d2
Kyle <59298462+Kfollen93@users.noreply.github.com>
2021-06-21 18:16:48 -0700

    Add C# program file

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiniReferenceGuide
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("A mini reference guide repository with C# code examples.");
+        }
+    }
+}
